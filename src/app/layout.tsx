eac82dc06efdfec14a634c315b2f26fc73b531dc
GlobalCVE <globalcve@gmail.com>
2025-10-24 16:34:12 +1100

    🧨 Favicon override: globalcve-favicon.ico to bypass Vercel cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon-v4.ico" />
+        <link rel="icon" href="/globalcve-favicon.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
